9da5aa7e5068d8090572dbacb48ddf6cc24d3704
hasan8733 <imtiazhasan873@gmail.com>
2026-02-13 06:06:01 +0500

    push

--- src/views/Dashboard/Properties/Properties.tsx
+++ src/views/Dashboard/Properties/Properties.tsx
@@ -103,14 +103,14 @@ const Properties: React.FC = () => {
 				</a>
 			),
 		},
-		// {
-		// 	key: "status",
-		// 	header: "Status",
-		// 	type: "text",
-		// 	render: (value: string) => {
-		// 		return <span className={`px-2 py-1 rounded-full text-xs font-semibold ${value === "Active" ? "bg-green-100 text-green-800" : value === "Inactive" ? "bg-red-100 text-red-800" : "bg-yellow-100 text-yellow-800"}`}>{value || "Inactive"}</span>;
-		// 	},
-		// },
+		{
+			key: "status",
+			header: "Status",
+			type: "text",
+			render: (value: string) => {
+				return <span className={`${value == "Active" ? "text-green-500" : value == "Inactive" ? "text-red-500" : "text-yellow-500"}`}>{value || "Inactive"}</span>;
+			},
+		},
 	];
 	const handleViewClick = (user: User) => {
 		setSelectedUser(user);
