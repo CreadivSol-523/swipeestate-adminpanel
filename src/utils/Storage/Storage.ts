693fc6f5bbb77994c53c369855bda2025fd6cd5d
hassan523 <hs542327@gmail.com>
2026-02-13 16:01:47 +0500

    push

--- src/utils/Storage/Storage.ts
+++ src/utils/Storage/Storage.ts
@@ -2,4 +2,5 @@ import { getCookie } from "cookies-next";
 
 export const cookiesData = getCookie("swipeestate") ? JSON?.parse(getCookie("swipeestate") as string) : undefined;
 export const Token = cookiesData?.data?.accessToken;
+export const RefreshToken = cookiesData?.data?.refreshToken;
 export const UserData = cookiesData?.data?.user;
